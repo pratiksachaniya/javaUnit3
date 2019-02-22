d06031ea8ce37151a3bdf19a96d453bd7ad8a8a2
Pratik Sachaniya <34182240+pratiksachaniya@users.noreply.github.com>
2019-02-22 08:36:29 +0530

    Add files via upload

---
+++ 3_1/DemoClass.java
@@ -0,0 +1,10 @@
+import P.*;
+class DemoClass
+{
+	public static void main(String ar[])
+	{
+		PackClass p1 = new PackClass();
+		
+		System.out.println("Max Is: " + p1.findMax(30,40,10));
+	}
+}
---
+++ 3_1/P/PackClass.java
@@ -0,0 +1,20 @@
+package P;
+public class PackClass
+{
+	public int findMax(int n1,int n2,int n3)
+	{
+		if(n1 > n2 && n1 > n3)
+		{
+			return n1;
+		}
+		else if(n2 > n3)
+		{
+			return n2;
+		}
+		else
+		{
+			return n3;
+		}
+	}
+	
+}
